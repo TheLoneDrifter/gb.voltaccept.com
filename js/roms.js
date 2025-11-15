2141ba4beee59fe26958c4fa3a499c1f0653ed77
The Lone Drifter <staydrifting@voltaccept.com>
2025-11-15 13:36:33 -0500

    v0.8.1

--- js/roms.js
+++ js/roms.js
@@ -3,12 +3,13 @@ if (document.location.href.includes("rom=")) {
     const urlParams = new URLSearchParams(document.location.search);
     const romParam = urlParams.get("rom").toUpperCase();
     const roms = ["V01D"];
+
     if (!roms.includes(romParam)) {
         throw new Error("Invalid ROM specified in URL parameter. Allowed values are: " + roms.join(", "));
     }
     if (romParam && romParam.length > 0) {
         window.ROM_FILENAME = "rom/" + romParam + ".gb";
         window.ROM_NAME = romParam;
-        document.head.querySelector("title").innerText = window.ROM_NAME;
+        document.head.querySelector("title").innerHTML = window.ROM_NAME;
     }
 }
