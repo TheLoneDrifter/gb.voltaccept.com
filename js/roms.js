660ae9a596e379027173edfe817f740cd4c190eb
The Lone Drifter <staydrifting@voltaccept.com>
2025-11-15 16:10:21 -0500

    v0.8.4

--- js/roms.js
+++ js/roms.js
@@ -1,15 +1,110 @@
 
-if (document.location.href.includes("rom=")) {
-    const urlParams = new URLSearchParams(document.location.search);
-    const romParam = urlParams.get("rom").toUpperCase();
-    const roms = ["V01D"];
+var romList = [
+    { name: "V01D", path: "rom/V01D.gb", model: "DMG-TRPG-USA" },
+];
 
-    if (!roms.includes(romParam)) {
-        throw new Error("Invalid ROM specified in URL parameter. Allowed values are: " + roms.join(", "));
+var cartridgeList = [];
+const urlParams = new URLSearchParams(window.location.search);
+var currentRom = null;
+
+if (urlParams.has('rom')) {
+    currentRom = urlParams.get('rom');
+}
+
+if (currentRom !== null) {
+    const rom = romList.find(r => r.name === currentRom);
+    if (rom) {
+        window.ROM_FILENAME = rom.path;
+        window.ROM_NAME = rom.name;
     }
-    if (romParam && romParam.length > 0) {
-        window.ROM_FILENAME = "rom/" + romParam + ".gb";
-        window.ROM_NAME = romParam;
-        document.head.querySelector("title").innerHTML = window.ROM_NAME;
+}
+
+function loadCartridge(rom) {
+    document.location.href = '?rom=' + rom;
+}
+
+function ejectCartridge() {
+    document.location.href = window.location.pathname;
+}
+
+function initCartridgeList() {
+    const container = document.querySelector('.cartridge-cont');
+    romList.forEach(rom => {
+        const cart = document.createElement('div');
+        cart.className = 'cartridge';
+        cart.onclick = () => loadCartridge(rom.name);
+        cart.style.cursor = 'pointer';
+        cart.id = `cart_${rom.name}`;
+        cart.innerHTML = cartridgeHTML("front", rom);
+        container.appendChild(cart);
+        cartridgeList.push({ name: rom.name, id: cart.id });
+    });
+}
+
+function initializeCartridge() {
+    initCartridgeList();
+
+    if (currentRom) {
+        const selectedCart = document.getElementById(`cart_${currentRom}`);
+        if (selectedCart) {
+            selectedCart.classList.add('selected-cartridge');
+            const insertedContainer = document.querySelector('.inserted-cartridge');
+            insertedContainer.innerHTML = `${cartridgeHTML("back", romList.find(r => r.name === currentRom))}`; 
+            insertedContainer.style.cursor = 'pointer';
+        }
+        else {
+            console.warn(`Cartridge for ROM "${currentRom}" not found.`);
+            selectedCart.classList.remove('selected-cartridge');
+        }
     }
+}
+
+function init() {
+    initializeCartridge();
+}
+
+function cartridgeHTML(side = "front", rom = { name: "V01D", model: "DMG-TRPG-USA" }) {
+    if (side === "front") {
+        return `
+              <div class="cartridge_top">
+                <div class="part part_1"></div>
+                <div class="part part_2">
+                  <div class="part stripe"></div>
+                  <div class="part stripe"></div>
+                  <div class="part stripe"></div>
+                  <div class="part stripe"></div>
+                  <div class="part stripe"></div>
+                </div>
+              </div>
+              <div class="part logo">
+                <p>Nintendo <span>Gameboy</span></p>
+              </div>
+              <div class="part large"></div>
+              <div class="part dark">
+                <div class="part"></div>
+              </div>
+              <div class="part dark image_bg">
+                <div class="part image">
+                  <img id="cartridge_image" src="svg/${rom.name}.svg" alt="${rom.name} Cartridge Label" />
+                </div>
+              </div>
+              <div class="part triangle"></div>
+              </div>`;
+    } else if (side === "back") {
+        return `
+              <div class="cartridge_top back">
+                <div class="part part_1 back"></div>
+                <div class="part part_2 back">
+                  <div class="part stripe back"></div>
+                  <div class="part stripe back"></div>
+                  <div class="part stripe back"></div>
+                  <div class="part stripe back"></div>
+                </div>
+              <div class="part logo back">
+                <p>MADE IN AMERICA</p>
+                <p class="patent">PAT. PEND</p>
+              </div>
+              </div>`;
+    }
+    return '';
 }
